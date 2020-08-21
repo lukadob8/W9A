194c992c7faf7b2442883cc52f1f68c4dd562d8b
Luka Dobrijevic <lukadob@Lukas-MacBook-Pro.local>
2020-08-20 19:54:07 -0600

    Chaged variables into arrays and made a loop that displays each users status

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-var userAge = 17;
-var is_subscribed = false;
+// var userAge = 19;
+// var is_subscribed = false;
 
 // if (userAge >= 18) {
 //     console.log ("User is 18 or older")
@@ -13,16 +13,35 @@ var is_subscribed = false;
 //     console.log ("User is not subscribed")
 // }
 
-if ((userAge >= 18) && is_subscribed == true) {
-    console.log ("User is 18 or older and is subscribed")
-} else if ((userAge < 18)&& is_subscribed == true) {
-    console.log ("User is younger than 18 and is subscribed")
-} else if ((userAge >= 18)&& is_subscribed != true) {
-    console.log ("User is 18 or older and is not subscribed")
-} else if ((userAge < 18)&& is_subscribed != true) {
-    console.log ("User is younger than 18 and is not subscribed")
-} else {
-    console.log ("A horrible error occured")
+var userAge = [15, 17, 19, 22, 25];
+var is_subscribed = [false, false, true, true, true];
+var arrayLength = userAge.length;
+
+for(var users = 0; users < arrayLength; users++) {
+    if ((userAge[users] >= 18) && is_subscribed[users] == true) {
+        console.log ("User is 18 or older and is subscribed")
+    } else if ((userAge[users] < 18) && is_subscribed[users] == true) {
+        console.log ("User is younger than 18 and is subscribed")
+    } else if ((userAge[users] >= 18) && is_subscribed[users] != true) {
+        console.log ("User is 18 or older and is not subscribed")
+    } else if ((userAge[users] < 18) && is_subscribed[users] != true) {
+        console.log ("User is younger than 18 and is not subscribed")
+    } else {
+        console.log ("A horrible error occured")
+    }
 }
+
+
+// if ((userAge >= 18) && is_subscribed == true) {
+//     console.log ("User is 18 or older and is subscribed")
+// } else if ((userAge < 18) && is_subscribed == true) {
+//     console.log ("User is younger than 18 and is subscribed")
+// } else if ((userAge >= 18) && is_subscribed != true) {
+//     console.log ("User is 18 or older and is not subscribed")
+// } else if ((userAge < 18) && is_subscribed != true) {
+//     console.log ("User is younger than 18 and is not subscribed")
+// } else {
+//     console.log ("A horrible error occured")
+// }
 
 
